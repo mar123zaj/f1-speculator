e540ba60c5d8e4b5b103d38e81b442179a06b55a
Marcin <alacris.1995@gmail.com>
2022-01-15 16:57:03 +0100

    chore: added pipes and filters to the application

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EntityNotFoundErrorFilter } from '@core/filters/entity-not-found.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new EntityNotFoundErrorFilter());
+
   await app.listen(3000);
 }
 bootstrap();

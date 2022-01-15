415da0eba9dba95cb529cfe75e7af60f3332cd6e
Marcin <alacris.1995@gmail.com>
2022-01-15 14:19:14 +0100

    refactor: cleared app.module file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,11 @@
 import { AuthModule } from '@auth/auth.module';
 import typeormConfig from '@config/typeorm.config';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
-console.log(__dirname)
+
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      
-      envFilePath: [__dirname + '../envs/backend.env'],
-    }),
     TypeOrmModule.forRoot({ ...typeormConfig, autoLoadEntities: true }),
     GraphQLModule.forRoot({ autoSchemaFile: true }),
     AuthModule,

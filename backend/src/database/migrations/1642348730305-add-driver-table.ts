1bba32eb0487e71ca4028bb5ea38d1c9fdc5fdb4
Marcin <alacris.1995@gmail.com>
2022-01-16 17:32:50 +0100

    refactor: removed constructor table from add-driver-table migration

--- backend/src/database/migrations/1642348730305-add-driver-table.ts
+++ backend/src/database/migrations/1642348730305-add-driver-table.ts
@@ -5,15 +5,11 @@ export class addDriverTable1642348730305 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'CREATE TABLE "constructor" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "nationality" character varying NOT NULL, CONSTRAINT "PK_4b7cb9ed6ddd155297d4b048eb2" PRIMARY KEY ("id"))'
-    );
-    await queryRunner.query(
       'CREATE TABLE "driver" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "surname" character varying NOT NULL, "birthDate" date NOT NULL, "nationality" character varying NOT NULL, CONSTRAINT "PK_61de71a8d217d585ecd5ee3d065" PRIMARY KEY ("id"))'
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('DROP TABLE "driver"');
-    await queryRunner.query('DROP TABLE "constructor"');
   }
 }
